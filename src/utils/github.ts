c55b6d2a4845ef0fd9cb4f11d09fb417a52241b0
Moonded <moonded@moonded.com>
2025-02-08 21:02:53 +0100

    improve and update github grapqhl

--- src/utils/github.ts
+++ src/utils/github.ts
@@ -20,147 +20,61 @@ export const app = new App({
 export const octokit = await app.getInstallationOctokit(
   Number(process.env.GITHUB_INSTALLATION_ID!)
 );
-// console.log(octokit);
 
-// export async function Updater() {
-//   try {
-//     const GraphData: GraphData = await octokit.graphql(
-//       `
-//     query {
-// red4ext: repository(owner: "WopsS", name: "RED4ext") {
-//     latestRelease {
-//       tagName
-//       updatedAt
-//       url
-//     }
-//   }
-//   archivexl: repository(owner: "psiberx", name: "cp2077-archive-xl") {
-//     latestRelease {
-//       tagName
-//       updatedAt
-//       url
-//     }
-//   }
-//   tweakxl: repository(owner: "psiberx", name: "cp2077-tweak-xl") {
-//     latestRelease {
-//       tagName
-//       updatedAt
-//       url
-//     }
-//   }
-//   codeware: repository(owner: "psiberx", name: "cp2077-codeware") {
-//     latestRelease {
-//       tagName
-//       updatedAt
-//       url
-//     }
-//   }
-//   cet: repository(owner: "maximegmd", name: "CyberEngineTweaks") {
-//     latestRelease {
-//       tagName
-//       updatedAt
-//       url
-//     }
-//   }
-//   redscript: repository(owner: "jac3km4", name: "redscript") {
-//     latestRelease {
-//       tagName
-//       updatedAt
-//       url
-//     }
-//   }
-// }
-//     `
-//     );
+export async function Updater() {
+  const data = await octokit.graphql(
+    `
+    query {
+red4ext: repository(owner: "WopsS", name: "RED4ext") {
+    latestRelease {
+      tagName
+      updatedAt
+      url
+    }
+  }
+  archivexl: repository(owner: "psiberx", name: "cp2077-archive-xl") {
+    latestRelease {
+      tagName
+      updatedAt
+      url
+    }
+  }
+  tweakxl: repository(owner: "psiberx", name: "cp2077-tweak-xl") {
+    latestRelease {
+      tagName
+      updatedAt
+      url
+    }
+  }
+  codeware: repository(owner: "psiberx", name: "cp2077-codeware") {
+    latestRelease {
+      tagName
+      updatedAt
+      url
+    }
+  }
+  cet: repository(owner: "maximegmd", name: "CyberEngineTweaks") {
+    latestRelease {
+      tagName
+      updatedAt
+      url
+    }
+  }
+  redscript: repository(owner: "jac3km4", name: "redscript") {
+    latestRelease {
+      tagName
+      updatedAt
+      url
+    }
+  }
+}
+   `
+  );
 
-//     const data = await prisma.github.upsert({
-//       where: {
-//         id: "1",
-//       },
-//       update: {
-//         red4ext: GraphData.red4ext,
-//         archivexl: GraphData.archivexl,
-//         tweakxl: GraphData.tweakxl,
-//         codeware: GraphData.codeware,
-//         cet: GraphData.cet,
-//         redscript: GraphData.redscript,
-//         updated: new Date(),
-//       },
-//       create: {
-//         id: "1",
-//         red4ext: GraphData.red4ext,
-//         archivexl: GraphData.archivexl,
-//         tweakxl: GraphData.tweakxl,
-//         codeware: GraphData.codeware,
-//         cet: GraphData.cet,
-//         redscript: GraphData.redscript,
-//         created: new Date(),
-//         updated: new Date(),
-//       },
-//     });
+  if (!data) return null;
 
-//     if (data) return true;
-//     return false;
-//   } catch (error) {
-//     console.error(error);
-//     return false;
-//   }
-// }
-
-// export async function GithubUserContributions(
-//   author: string,
-//   repo: string = "wolvenkit"
-// ): Promise<GithubData> {
-//   if (!author) return null;
-
-//   try {
-//     const RedModdingRepos = await octokit.rest.repos.listForUser({
-//       username: repo,
-//     });
-
-//     const RepoIds = RedModdingRepos.data.map((repo) => {
-//       return repo.node_id;
-//     });
-
-//     const GithubUserByName = await octokit.rest.users.getByUsername({
-//       username: author,
-//     });
-
-//     const GithubUserId = GithubUserByName.data.node_id;
-//     const data = await octokit.graphql(
-//       `
-//       query ($repos: [ID!]!, $author: String!, $authorId: ID!) {
-//           nodes(ids: $repos) {
-//             ... on Repository {
-//               nameWithOwner
-//               issues(filterBy: { createdBy: $author }) {
-//                 totalCount
-//               }
-//               defaultBranchRef {
-//                 target {
-//                   ... on Commit {
-//                     history(author: { id: $authorId }) {
-//                       totalCount
-//                     }
-//                   }
-//                 }
-//               }
-//             }
-//           }
-//         }
-//       `,
-//       {
-//         author: author,
-//         authorId: GithubUserId,
-//         repos: RepoIds,
-//       }
-//     );
-
-//     return data;
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
+  return data;
+}
 
 export async function GithubQuery(
   author: string | null

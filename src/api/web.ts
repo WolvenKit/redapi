d92a2aca34caf4f45f71a6a72e404791b0a4195e
Moonded <moonded@moonded.com>
2025-02-09 22:34:54 +0100

    query user for mod panel

--- src/api/web.ts
+++ src/api/web.ts
@@ -1,19 +1,31 @@
 import { Elysia } from "elysia";
-import { prisma } from "../utils/prisma";
+import { prisma, log } from "../utils";
 
 export const web = new Elysia({ prefix: "/web" })
-  .get("/",  async () => {
+  .get("/", async () => {
     return await prisma.user.findMany({
       take: 10,
       orderBy: {
-        GlobalId: "desc"
-      }
-    })
-  })
-  .get("/user/:userId", async ({params}) => {
-    return await prisma.user.findUnique({
-      where: {
-        GlobalId: Number(params.userId)
+        GlobalId: "desc",
       },
     });
   })
+  .get("/user/:userId", async ({ params }) => {
+    return await prisma.user.findUnique({
+      where: {
+        GlobalId: Number(params.userId),
+      },
+    });
+  })
+  .get("/user/moderation/:userId", async ({ params }) => {
+    const moderation = await prisma.moderation.findUnique({
+      where: {
+        DiscordId: params.userId,
+      },
+      include: { Warnings: true }, // Fetch related warnings
+    });
+
+    log(moderation);
+
+    return moderation;
+  });

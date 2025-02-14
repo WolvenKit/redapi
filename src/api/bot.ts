ffbf142d23acf745368fef91946bb69c1ec86564
Moonded <moonded@moonded.com>
2025-02-14 12:00:27 +0100

    add auth for Bot endpoints - predefined Bearer token

--- src/api/bot.ts
+++ src/api/bot.ts
@@ -1,122 +1,134 @@
 import { Elysia, t } from "elysia";
 import { prisma } from "../utils/prisma";
 import { NexusQuery, GithubQuery, log, Updater } from "../utils";
+import bearer from "@elysiajs/bearer";
 
-export const bot = new Elysia({ prefix: "/bot" })
-  .post(
-    "/",
-    async ({ body }) => {
-      log(body);
-
-      const NexusMods: Record<string, any> | undefined = await NexusQuery(
-        body.nexusmods
-      );
-      const Github: any = (await GithubQuery(body.github)) ?? undefined;
-
-      return await prisma.user.upsert({
-        where: {
-          DiscordiD: body.id,
-        },
-        update: {
-          Username: body.user,
-          GlobalName: body.globalname,
-          Avatar: body.image,
-          Theme: body.theme,
-          Style: body.namestyle,
-          Description: body.description,
-          GithubUsername: body.github,
-          NexusMods: NexusMods,
-          Github: Github,
-          Roles: body.Roles,
-        },
-        create: {
-          DiscordiD: body.id,
-          Username: body.user,
-          GlobalName: body.globalname,
-          Avatar: body.image,
-          Theme: body.theme,
-          Style: body.namestyle,
-          Description: body.description,
-          GithubUsername: body.github,
-          NexusModsUsername: body.nexusmods,
-          NexusMods: NexusMods,
-          Github: Github,
-          Roles: body.Roles,
-        },
-      });
+export const bot = new Elysia({ prefix: "/bot" }).use(bearer()).guard(
+  {
+    beforeHandle({ set, bearer }) {
+      if (bearer !== process.env.BOT_TOKEN) {
+        set.status = 401;
+        return "Unauthorized";
+      }
     },
-    {
-      body: t.Object({
-        server: t.Union([t.String(), t.Null()]),
-        id: t.String(),
-        user: t.String(),
-        globalname: t.Union([t.String(), t.Null()]),
-        image: t.Union([t.String(), t.Null()]),
-        nexusmods: t.Union([t.String(), t.Null()]),
-        github: t.Union([t.String(), t.Null()]),
-        theme: t.Union([t.String(), t.Null()]),
-        description: t.Union([t.String(), t.Null()]),
-        namestyle: t.Union([t.String(), t.Null()]),
-        Roles: t.Array(t.String()),
-      }),
-    }
-  )
-  .post("/commands", async ({ body }) => {}, {
-    body: t.Object({
-      command: t.String(),
-      response: t.String(),
-    }),
-  })
-  .post(
-    "/quotes",
-    async ({ body }) => {
-      return await prisma.quotes.create({
-        data: {
-          Quote: body.Quote,
-          Responder: body.Responder,
-        },
-      });
-    },
-    {
-      body: t.Object({
-        Quote: t.String(),
-        Responder: t.String(),
-      }),
-    }
-  )
-  .post(
-    "/quotes/request",
-    async ({ body }) => {
-      const Responder = body.Responder;
+  },
+  (app) =>
+    app
+      .post(
+        "/",
+        async ({ body }) => {
+          log(body);
 
-      console.log(Responder || "everyone");
+          const NexusMods: Record<string, any> | undefined = await NexusQuery(
+            body.nexusmods
+          );
+          const Github: any = (await GithubQuery(body.github)) ?? undefined;
 
-      const productsCount = await prisma.quotes.count();
-      const skip = Math.floor(Math.random() * productsCount);
-      return await prisma.quotes.findMany({
-        take: 1,
-        skip: skip,
-        orderBy: {
-          GlobalId: "asc",
-        },
-        where: {
-          OR: [
-            {
-              Responder: Responder,
+          return await prisma.user.upsert({
+            where: {
+              DiscordiD: body.id,
             },
-            {
-              Responder: "everyone",
+            update: {
+              Username: body.user,
+              GlobalName: body.globalname,
+              Avatar: body.image,
+              Theme: body.theme,
+              Style: body.namestyle,
+              Description: body.description,
+              GithubUsername: body.github,
+              NexusMods: NexusMods,
+              Github: Github,
+              Roles: body.Roles,
             },
-          ],
+            create: {
+              DiscordiD: body.id,
+              Username: body.user,
+              GlobalName: body.globalname,
+              Avatar: body.image,
+              Theme: body.theme,
+              Style: body.namestyle,
+              Description: body.description,
+              GithubUsername: body.github,
+              NexusModsUsername: body.nexusmods,
+              NexusMods: NexusMods,
+              Github: Github,
+              Roles: body.Roles,
+            },
+          });
         },
-      });
-    },
-    {
-      body: t.Object({
-        Responder: t.String(),
-      }),
-    }
-  )
-  .get("/coreversions", async () => {
-    return await Updater();
-  });
+        {
+          body: t.Object({
+            server: t.Union([t.String(), t.Null()]),
+            id: t.String(),
+            user: t.String(),
+            globalname: t.Union([t.String(), t.Null()]),
+            image: t.Union([t.String(), t.Null()]),
+            nexusmods: t.Union([t.String(), t.Null()]),
+            github: t.Union([t.String(), t.Null()]),
+            theme: t.Union([t.String(), t.Null()]),
+            description: t.Union([t.String(), t.Null()]),
+            namestyle: t.Union([t.String(), t.Null()]),
+            Roles: t.Array(t.String()),
+          }),
+        }
+      )
+      .post("/commands", async ({ body }) => {}, {
+        body: t.Object({
+          command: t.String(),
+          response: t.String(),
+        }),
+      })
+      .post(
+        "/quotes",
+        async ({ body }) => {
+          return await prisma.quotes.create({
+            data: {
+              Quote: body.Quote,
+              Responder: body.Responder,
+            },
+          });
+        },
+        {
+          body: t.Object({
+            Quote: t.String(),
+            Responder: t.String(),
+          }),
+        }
+      )
+      .post(
+        "/quotes/request",
+        async ({ body }) => {
+          const Responder = body.Responder;
+
+          console.log(Responder || "everyone");
+
+          const productsCount = await prisma.quotes.count();
+          const skip = Math.floor(Math.random() * productsCount);
+          return await prisma.quotes.findMany({
+            take: 1,
+            skip: skip,
+            orderBy: {
+              GlobalId: "asc",
+            },
+            where: {
+              OR: [
+                {
+                  Responder: Responder,
+                },
+                {
+                  Responder: "everyone",
+                },
+              ],
+            },
+          });
+        },
+        {
+          body: t.Object({
+            Responder: t.String(),
+          }),
+        }
+      )
+      .get("/coreversions", async () => {
+        return await Updater();
+      })
+);
--- src/api/moderation.ts
+++ src/api/moderation.ts
@@ -1,262 +1,274 @@
 import { Elysia, t } from "elysia";
 import { prisma } from "../utils/prisma";
 import { log } from "../utils";
+import { bearer } from "@elysiajs/bearer";
 
 function calculateWarningLevel(warningCount: number): number {
   return Math.min(100, 10 * Math.pow(1.5, warningCount));
 }
 
 export const moderation = new Elysia({ prefix: "/moderation" })
-  .post(
-    "/ban",
-    async ({ body }) => {
-      const moderation = await prisma.moderation.findUnique({
-        where: { DiscordId: body.DiscordId },
-        include: { Warnings: true }, // Fetch related warnings
-      });
-
-      // Count previous warnings
-      const previousWarnings = moderation?.WarningCount ?? 0;
-      const updatedWarnings = previousWarnings + 1;
-
-      // Calculate new warning level
-      const newWarningLevel = calculateWarningLevel(updatedWarnings);
-
-      const bans = await prisma.bans.create({
-        data: {
-          DiscordId: body.DiscordId,
-          DiscordName: body.User,
-          Reason: body.Reason,
-          Partial: newWarningLevel > 75.94 ? false : true,
-          GuildId: body.GuildId,
-        },
-      });
-
-      const newmoderation = await prisma.moderation.upsert({
-        where: {
-          DiscordId: body.DiscordId,
-        },
-        update: {
-          DiscordId: body.DiscordId,
-          WarningCount: {
-            increment: 1,
-          },
-          WarningLevel: newWarningLevel,
-          Status: body.Type,
-          LastReason: body.Reason,
-        },
-        create: {
-          DiscordId: body.DiscordId,
-          Status: body.Type,
-          LastReason: body.Reason,
-          Resolved: false,
-          WarningCount: 1,
-          WarningLevel: newWarningLevel,
-        },
-      });
-
-      log(newmoderation, bans);
-
-      return [body, newWarningLevel];
-    },
+  .use(bearer())
+  .guard(
     {
-      body: t.Object({
-        Type: t.String(),
-        DiscordId: t.String(),
-        User: t.String(),
-        Reason: t.String(),
-        Issuer: t.String(),
-        GuildId: t.String(),
-      }),
-    }
-  )
-  .post(
-    "/warn",
-    async ({ body }) => {
-      const moderation = await prisma.moderation.findUnique({
-        where: { DiscordId: body.DiscordId },
-        include: { Warnings: true }, // Fetch related warnings
-      });
-
-      // Count previous warnings
-      const previousWarnings = moderation?.WarningCount ?? 0;
-      const updatedWarnings = previousWarnings + 1;
-
-      // Calculate new warning level
-      const newWarningLevel = calculateWarningLevel(updatedWarnings);
-
-      const newModeration = await prisma.moderation.upsert({
-        where: {
-          DiscordId: body.DiscordId,
-        },
-        update: {
-          DiscordId: body.DiscordId,
-          Status: body.Type,
-          LastReason: body.Reason,
-          LastWarning: new Date().toISOString(),
-          Resolved: false,
-          WarningCount: {
-            increment: 1,
-          },
-          WarningLevel: newWarningLevel,
-          Warnings: {
-            create: {
-              Reason: body.Reason,
-              IssuedTime: new Date(),
-              IssuerName: body.Issuer,
-            },
-          },
-        },
-        create: {
-          DiscordId: body.DiscordId,
-          Status: body.Type,
-          LastReason: body.Reason,
-          LastWarning: new Date().toISOString(),
-          Resolved: false,
-          WarningCount: 1,
-          WarningLevel: newWarningLevel,
-          Warnings: {
-            create: {
-              Reason: body.Reason,
-              IssuedTime: new Date(),
-              IssuerName: body.Issuer,
-            },
-          },
-        },
-      });
-
-      log(newModeration);
-
-      return [body, newWarningLevel];
+      beforeHandle({ set, bearer }) {
+        if (bearer !== process.env.BOT_TOKEN) {
+          set.status = 401;
+          return "Unauthorized";
+        }
+      },
     },
-    {
-      body: t.Object({
-        Type: t.String(),
-        DiscordId: t.String(),
-        User: t.String(),
-        Reason: t.String(),
-        Issuer: t.String(),
-      }),
-    }
-  )
-  .post(
-    "/kick",
-    async ({ body }) => {
-      const kick = await prisma.kick.create({
-        data: {
-          DiscordId: body.DiscordId,
-          DiscordName: body.User,
-          Reason: body.Reason,
-          Partial: true,
-          GuildId: body.GuildId,
-        },
-      });
+    (app) =>
+      app
+        .post(
+          "/ban",
+          async ({ body }) => {
+            const moderation = await prisma.moderation.findUnique({
+              where: { DiscordId: body.DiscordId },
+              include: { Warnings: true }, // Fetch related warnings
+            });
 
-      const moderation = await prisma.moderation.findUnique({
-        where: { DiscordId: body.DiscordId },
-        include: { Warnings: true }, // Fetch related warnings
-      });
+            // Count previous warnings
+            const previousWarnings = moderation?.WarningCount ?? 0;
+            const updatedWarnings = previousWarnings + 1;
 
-      console.log(moderation);
+            // Calculate new warning level
+            const newWarningLevel = calculateWarningLevel(updatedWarnings);
 
-      // Count previous warnings
-      const previousWarnings = moderation?.WarningCount ?? 0;
-      const updatedWarnings = previousWarnings + 1;
+            const bans = await prisma.bans.create({
+              data: {
+                DiscordId: body.DiscordId,
+                DiscordName: body.User,
+                Reason: body.Reason,
+                Partial: newWarningLevel > 75.94 ? false : true,
+                GuildId: body.GuildId,
+              },
+            });
 
-      // Calculate new warning level
-      const newWarningLevel = calculateWarningLevel(updatedWarnings);
+            const newmoderation = await prisma.moderation.upsert({
+              where: {
+                DiscordId: body.DiscordId,
+              },
+              update: {
+                DiscordId: body.DiscordId,
+                WarningCount: {
+                  increment: 1,
+                },
+                WarningLevel: newWarningLevel,
+                Status: body.Type,
+                LastReason: body.Reason,
+              },
+              create: {
+                DiscordId: body.DiscordId,
+                Status: body.Type,
+                LastReason: body.Reason,
+                Resolved: false,
+                WarningCount: 1,
+                WarningLevel: newWarningLevel,
+              },
+            });
 
-      console.log(newWarningLevel);
+            log(newmoderation, bans);
 
-      const Newmoderation = await prisma.moderation.upsert({
-        where: {
-          DiscordId: body.DiscordId,
-        },
-        update: {
-          DiscordId: body.DiscordId,
-          WarningCount: {
-            increment: 1,
+            return [body, newWarningLevel];
           },
-          WarningLevel: newWarningLevel,
-          Status: body.Type,
-          LastReason: body.Reason,
-          LastWarning: new Date(),
-          Resolved: false,
-        },
-        create: {
-          DiscordId: body.DiscordId,
-          Status: body.Type,
-          LastReason: body.Reason,
-          Resolved: false,
-          WarningCount: 1,
-          WarningLevel: newWarningLevel,
-          LastWarning: new Date(),
-        },
-      });
+          {
+            body: t.Object({
+              Type: t.String(),
+              DiscordId: t.String(),
+              User: t.String(),
+              Reason: t.String(),
+              Issuer: t.String(),
+              GuildId: t.String(),
+            }),
+          }
+        )
+        .post(
+          "/warn",
+          async ({ body }) => {
+            const moderation = await prisma.moderation.findUnique({
+              where: { DiscordId: body.DiscordId },
+              include: { Warnings: true }, // Fetch related warnings
+            });
 
-      //   console.log(Newmoderation);
+            // Count previous warnings
+            const previousWarnings = moderation?.WarningCount ?? 0;
+            const updatedWarnings = previousWarnings + 1;
 
-      return [body, newWarningLevel];
-    },
-    {
-      body: t.Object({
-        Type: t.String(),
-        DiscordId: t.String(),
-        User: t.String(),
-        Reason: t.String(),
-        Issuer: t.String(),
-        GuildId: t.String(),
-      }),
-    }
-  )
-  .post(
-    "/drop",
-    async ({ body }) => {
-      // Drop all Moderation data for a user
+            // Calculate new warning level
+            const newWarningLevel = calculateWarningLevel(updatedWarnings);
 
-      const moderation = await prisma.moderation
-        .deleteMany({
-          where: {
-            DiscordId: body.DiscordId,
+            const newModeration = await prisma.moderation.upsert({
+              where: {
+                DiscordId: body.DiscordId,
+              },
+              update: {
+                DiscordId: body.DiscordId,
+                Status: body.Type,
+                LastReason: body.Reason,
+                LastWarning: new Date().toISOString(),
+                Resolved: false,
+                WarningCount: {
+                  increment: 1,
+                },
+                WarningLevel: newWarningLevel,
+                Warnings: {
+                  create: {
+                    Reason: body.Reason,
+                    IssuedTime: new Date(),
+                    IssuerName: body.Issuer,
+                  },
+                },
+              },
+              create: {
+                DiscordId: body.DiscordId,
+                Status: body.Type,
+                LastReason: body.Reason,
+                LastWarning: new Date().toISOString(),
+                Resolved: false,
+                WarningCount: 1,
+                WarningLevel: newWarningLevel,
+                Warnings: {
+                  create: {
+                    Reason: body.Reason,
+                    IssuedTime: new Date(),
+                    IssuerName: body.Issuer,
+                  },
+                },
+              },
+            });
+
+            log(newModeration);
+
+            return [body, newWarningLevel];
           },
-        })
-        .catch((error) => {
-          return error;
-        });
+          {
+            body: t.Object({
+              Type: t.String(),
+              DiscordId: t.String(),
+              User: t.String(),
+              Reason: t.String(),
+              Issuer: t.String(),
+            }),
+          }
+        )
+        .post(
+          "/kick",
+          async ({ body }) => {
+            await prisma.kick.create({
+              data: {
+                DiscordId: body.DiscordId,
+                DiscordName: body.User,
+                Reason: body.Reason,
+                Partial: true,
+                GuildId: body.GuildId,
+              },
+            });
 
-      // Drop all Kicks for a user
+            const moderation = await prisma.moderation.findUnique({
+              where: { DiscordId: body.DiscordId },
+              include: { Warnings: true }, // Fetch related warnings
+            });
 
-      const kicks = await prisma.kick
-        .deleteMany({
-          where: {
-            DiscordId: body.DiscordId,
+            console.log(moderation);
+
+            // Count previous warnings
+            const previousWarnings = moderation?.WarningCount ?? 0;
+            const updatedWarnings = previousWarnings + 1;
+
+            // Calculate new warning level
+            const newWarningLevel = calculateWarningLevel(updatedWarnings);
+
+            console.log(newWarningLevel);
+
+            await prisma.moderation.upsert({
+              where: {
+                DiscordId: body.DiscordId,
+              },
+              update: {
+                DiscordId: body.DiscordId,
+                WarningCount: {
+                  increment: 1,
+                },
+                WarningLevel: newWarningLevel,
+                Status: body.Type,
+                LastReason: body.Reason,
+                LastWarning: new Date(),
+                Resolved: false,
+              },
+              create: {
+                DiscordId: body.DiscordId,
+                Status: body.Type,
+                LastReason: body.Reason,
+                Resolved: false,
+                WarningCount: 1,
+                WarningLevel: newWarningLevel,
+                LastWarning: new Date(),
+              },
+            });
+
+            return [body, newWarningLevel];
           },
-        })
-        .catch((error) => {
-          return error;
-        });
+          {
+            body: t.Object({
+              Type: t.String(),
+              DiscordId: t.String(),
+              User: t.String(),
+              Reason: t.String(),
+              Issuer: t.String(),
+              GuildId: t.String(),
+            }),
+          }
+        )
+        .post(
+          "/drop",
+          async ({ body }) => {
+            // Drop all Moderation data for a user
 
-      // Drop all Bans for a user
+            const moderation = await prisma.moderation
+              .deleteMany({
+                where: {
+                  DiscordId: body.DiscordId,
+                },
+              })
+              .catch((error) => {
+                return error;
+              });
 
-      const bans = await prisma.bans
-        .deleteMany({
-          where: {
-            DiscordId: body.DiscordId,
+            // Drop all Kicks for a user
+
+            const kicks = await prisma.kick
+              .deleteMany({
+                where: {
+                  DiscordId: body.DiscordId,
+                },
+              })
+              .catch((error) => {
+                return error;
+              });
+
+            // Drop all Bans for a user
+
+            const bans = await prisma.bans
+              .deleteMany({
+                where: {
+                  DiscordId: body.DiscordId,
+                },
+              })
+              .catch((error) => {
+                return error;
+              });
+
+            if (!moderation || !kicks || !bans) {
+              return body;
+            }
+
+            return [body, moderation, kicks, bans];
           },
-        })
-        .catch((error) => {
-          return error;
-        });
-
-      if (!moderation || !kicks || !bans) {
-        return body;
-      }
-
-      return [body, moderation, kicks, bans];
-    },
-    {
-      body: t.Object({
-        DiscordId: t.String(),
-      }),
-    }
+          {
+            body: t.Object({
+              DiscordId: t.String(),
+            }),
+          }
+        )
   );

5babd69dec4d571e9935e29ff4ab645d2d73da0a
Moonded <moonded@moonded.com>
2025-02-14 11:59:05 +0100

    new endpoints: - moderation/bans - moderation/kicks - moderation/:userid #needs auth beforehand

--- src/api/web.ts
+++ src/api/web.ts
@@ -1,5 +1,5 @@
-import { Elysia } from "elysia";
-import { prisma, log } from "../utils";
+import { Elysia, t } from "elysia";
+import { prisma } from "../utils";
 
 export const web = new Elysia({ prefix: "/web" })
   .get("/", async () => {
@@ -10,22 +10,83 @@ export const web = new Elysia({ prefix: "/web" })
       },
     });
   })
-  .get("/user/:userId", async ({ params }) => {
-    return await prisma.user.findUnique({
-      where: {
-        GlobalId: Number(params.userId),
-      },
-    });
-  })
-  .get("/user/moderation/:userId", async ({ params }) => {
-    const moderation = await prisma.moderation.findUnique({
-      where: {
-        DiscordId: params.userId,
-      },
-      include: { Warnings: true }, // Fetch related warnings
-    });
+  .get(
+    "/user/:userId",
+    async ({ params }) => {
+      return await prisma.user.findUnique({
+        where: {
+          GlobalId: Number(params.userId),
+        },
+      });
+    },
+    {
+      params: t.Object({
+        userId: t.String(),
+      }),
+    }
+  )
+  .get(
+    "/moderation/bans",
+    async ({ query }) => {
+      return await prisma.bans.findMany({
+        take: 10,
+        skip: (query.page ? Number(query.page) : 1 - 1) * 10,
+        orderBy: {
+          GlobalId: "desc",
+        },
+      });
+    },
+    {
+      query: t.Object({
+        page: t.Optional(t.String()),
+      }),
+    }
+  )
+  .get(
+    "/moderation/kicks",
+    async ({ query }) => {
+      return await prisma.kick.findMany({
+        take: 10,
+        skip: (query.page ? Number(query.page) : 1 - 1) * 10,
+        orderBy: {
+          GlobalId: "desc",
+        },
+      });
+    },
+    {
+      query: t.Object({
+        page: t.Optional(t.String()),
+      }),
+    }
+  )
+  .get(
+    "/moderation/:userId",
+    async ({ params, cookie: { auth }, error, redirect }) => {
+      if (!auth.value) {
+        redirect("/sign");
+      }
 
-    log(moderation);
+      const user = await prisma.moderation.findUnique({
+        where: {
+          DiscordId: params.userId,
+          JWT: auth.value,
+        },
+        omit: {
+          JWT: true,
+          GlobalId: true,
+        },
+      });
 
-    return moderation;
-  });
+      return user ?? error(401);
+    },
+    {
+      params: t.Object({
+        userId: t.String(),
+      }),
+      cookie: t.Object({
+        userData: t.String(),
+        discord: t.String(),
+        auth: t.String(),
+      }),
+    }
+  );

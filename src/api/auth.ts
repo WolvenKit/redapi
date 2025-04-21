46fa9584e462f2ae92ffede7ee4b81c4af2f10e1
Moonded <moonded@moonded.com>
2025-04-21 16:14:09 +0200

    fixed no 1 of gits fucking bullshit

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -2,139 +2,159 @@ import { Elysia, t } from "elysia";
 import { log, prisma } from "../utils";
 import jwt from "@elysiajs/jwt";
 import { defaultPermission, AdminPermission } from "../utils";
+import { bearer } from "@elysiajs/bearer";
+import { ValidateAuth } from "../middleware/auth";
 
-export const auth = new Elysia({ prefix: "/auth" })
-  .use(
-    jwt({
-      name: "jwt",
-      secret: process.env.JWT_SECRET!,
-    }),
-  )
-  .get("/login", async ({ redirect }) => {
-    log("Redirecting to Discord OAuth2");
-    const URL = new URLSearchParams({
-      client_id: "796283112199553044",
-      response_type: "code",
-      redirect_uri: `${process.env.HOSTNAME_METHOD}://${process.env.HOSTNAME}${
-        process.env.PORT ? ":" + process.env.PORT : ":" + 8080
-      }/auth/redirect`,
-      scope: "identify guilds email",
-    });
-
-    return redirect(`https://discord.com/oauth2/authorize?${URL.toString()}`);
+export const auth = new Elysia({ prefix: "/auth" }).use(
+  jwt({
+    name: "jwt",
+    secret: process.env.JWT_SECRET!,
   })
-  .get(
-    "/redirect",
-    async ({ jwt, query, cookie: { userData, discord, auth }, redirect }) => {
-      const code = query.code;
-
-      const data = await fetch("https://discord.com/api/oauth2/token", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
+    .use(bearer())
+    .guard(
+      {
+        beforeHandle({ set, bearer, error, path }) {
+          return ValidateAuth(set, bearer, error, path, "moderation");
         },
-        body: new URLSearchParams({
-          client_id: "796283112199553044",
-          client_secret: process.env.DISCORD_SECRET!,
-          grant_type: "authorization_code",
-          code: code,
-          redirect_uri: `${process.env.HOSTNAME_METHOD}://${
-            process.env.HOSTNAME
-          }${
-            process.env.PORT ? ":" + process.env.PORT : ":" + 8080
-          }/auth/redirect`,
-          scope: "identify guilds user email",
-        }),
-      });
+      },
+      (app) =>
+        app
+          .get("/login", async ({ redirect }) => {
+            log("Redirecting to Discord OAuth2");
+            const URL = new URLSearchParams({
+              client_id: "796283112199553044",
+              response_type: "code",
+              redirect_uri: `${process.env.HOSTNAME_METHOD}://${
+                process.env.HOSTNAME
+              }${
+                process.env.PORT ? ":" + process.env.PORT : ":" + 8080
+              }/auth/redirect`,
+              scope: "identify guilds email",
+            });
 
-      const json: Token = await data.json();
+            return redirect(
+              `https://discord.com/oauth2/authorize?${URL.toString()}`
+            );
+          })
+          .get(
+            "/redirect",
+            async ({
+              jwt,
+              query,
+              cookie: { userData, discord, auth },
+              redirect,
+            }) => {
+              const code = query.code;
 
-      userData.set({
-        value: JSON.stringify(json),
-        path: "/",
-        httpOnly: false,
-        maxAge: 3600,
-      });
+              const data = await fetch("https://discord.com/api/oauth2/token", {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/x-www-form-urlencoded",
+                },
+                body: new URLSearchParams({
+                  client_id: "796283112199553044",
+                  client_secret: process.env.DISCORD_SECRET!,
+                  grant_type: "authorization_code",
+                  code: code,
+                  redirect_uri: `${process.env.HOSTNAME_METHOD}://${
+                    process.env.HOSTNAME
+                  }${
+                    process.env.PORT ? ":" + process.env.PORT : ":" + 8080
+                  }/auth/redirect`,
+                  scope: "identify guilds user email",
+                }),
+              });
 
-      const user = await fetch("https://discord.com/api/users/@me", {
-        headers: {
-          Authorization: `Bearer ${json.access_token}`,
-        },
-      });
+              const json: Token = await data.json();
 
-      const DiscordUser: User = await user.json();
+              userData.set({
+                value: JSON.stringify(json),
+                path: "/",
+                httpOnly: false,
+                maxAge: 3600,
+              });
 
-      if (DiscordUser.id === (process.env.ADMIN_ID as string)) {
-        await prisma.auth.upsert({
-          where: {
-            DiscordId: DiscordUser.id,
-          },
-          update: {},
-          create: {
-            DiscordId: DiscordUser.id,
-            JWT: await jwt.sign({
-              id: DiscordUser.id,
-              username: DiscordUser.username,
-              discriminator: DiscordUser.discriminator,
-            }),
-            Endpoints: AdminPermission,
-          },
-        });
-      } else {
-        await prisma.auth.upsert({
-          where: {
-            DiscordId: DiscordUser.id,
-          },
-          update: {},
-          create: {
-            DiscordId: DiscordUser.id,
-            JWT: await jwt.sign({
-              id: DiscordUser.id,
-              username: DiscordUser.username,
-              discriminator: DiscordUser.discriminator,
-            }),
-            Endpoints: defaultPermission,
-          },
-        });
-      }
-      auth.set({
-        value: await jwt.sign({
-          id: DiscordUser.id,
-          username: DiscordUser.username,
-        }),
-        httpOnly: false,
-        maxAge: 7 * 86400,
-        path: process.env.INTERFACE_URL as string,
-      });
+              const user = await fetch("https://discord.com/api/users/@me", {
+                headers: {
+                  Authorization: `Bearer ${json.access_token}`,
+                },
+              });
 
-      await prisma.moderation.upsert({
-        where: {
-          DiscordId: DiscordUser.id,
-        },
-        update: {
-          DiscordId: DiscordUser.id,
-          JWT: auth.value,
-        },
-        create: {
-          DiscordId: DiscordUser.id,
-          JWT: auth.value,
-          Resolved: true,
-        },
-      });
+              const DiscordUser: User = await user.json();
 
-      // save user data to a cookie
-      discord.set({
-        value: JSON.stringify(DiscordUser),
-        path: "/",
-        httpOnly: false,
-        maxAge: 3600,
-      });
+              if (DiscordUser.id === (process.env.ADMIN_ID as string)) {
+                await prisma.auth.upsert({
+                  where: {
+                    DiscordId: DiscordUser.id,
+                  },
+                  update: {},
+                  create: {
+                    DiscordId: DiscordUser.id,
+                    JWT: await jwt.sign({
+                      id: DiscordUser.id,
+                      username: DiscordUser.username,
+                      discriminator: DiscordUser.discriminator,
+                    }),
+                    Endpoints: AdminPermission,
+                  },
+                });
+              } else {
+                await prisma.auth.upsert({
+                  where: {
+                    DiscordId: DiscordUser.id,
+                  },
+                  update: {},
+                  create: {
+                    DiscordId: DiscordUser.id,
+                    JWT: await jwt.sign({
+                      id: DiscordUser.id,
+                      username: DiscordUser.username,
+                      discriminator: DiscordUser.discriminator,
+                    }),
+                    Endpoints: defaultPermission,
+                  },
+                });
+              }
+              auth.set({
+                value: await jwt.sign({
+                  id: DiscordUser.id,
+                  username: DiscordUser.username,
+                }),
+                httpOnly: false,
+                maxAge: 7 * 86400,
+                path: process.env.INTERFACE_URL as string,
+              });
 
-      return redirect(process.env.REDIRECT as string);
-    },
-    {
-      query: t.Object({
-        code: t.String(),
-      }),
-    },
-  );
+              await prisma.moderation.upsert({
+                where: {
+                  DiscordId: DiscordUser.id,
+                },
+                update: {
+                  DiscordId: DiscordUser.id,
+                  JWT: auth.value,
+                },
+                create: {
+                  DiscordId: DiscordUser.id,
+                  JWT: auth.value,
+                  Resolved: true,
+                },
+              });
+
+              // save user data to a cookie
+              discord.set({
+                value: JSON.stringify(DiscordUser),
+                path: "/",
+                httpOnly: false,
+                maxAge: 3600,
+              });
+
+              return redirect(process.env.REDIRECT as string);
+            },
+            {
+              query: t.Object({
+                code: t.String(),
+              }),
+            }
+          )
+    )
+);
